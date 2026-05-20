cb74a4088783de386633b3d570d9be519625889e
Timur Usmanov <t.usmanov@innopolis.university>
2026-05-20 11:55:02 +0300

    Add rule for flex

--- cpp-classic/cpp-classic.mk
+++ cpp-classic/cpp-classic.mk
@@ -1,5 +1,10 @@
 cpp-classic/Absyn.C cpp-classic/Absyn.H cpp-classic/Buffer.C cpp-classic/Buffer.H cpp-classic/grammar.l cpp-classic/grammar.y cpp-classic/ParserError.H cpp-classic/Parser.H cpp-classic/Printer.C cpp-classic/Printer.H cpp-classic/Skeleton.C cpp-classic/Skeleton.H cpp-classic/Test.C &: grammar.cf | cpp-classic
 	cd cpp-classic && bnfc -p LC --cpp ../grammar.cf
 
+cpp-classic/lex.LC.c: cpp-classic/grammar.l | cpp-classic
+	cd cpp-classic && flex grammar.l
+
+cpp-classic/
+
 cpp-classic:
 	mkdir cpp-classic
