45869a125b4058312d544d04c62762fdf96b7aa3
Timur Usmanov <t.usmanov@innopolis.university>
2026-05-22 20:00:24 +0300

    Add Absyn.cpp

---
+++ desired/Absyn.cpp
@@ -0,0 +1,54 @@
+#include "Absyn.hpp"
+#include <memory>
+
+namespace LC {
+
+AProgram::AProgram(const AProgram& other)
+    : ListExpr_(other.ListExpr_) {}
+
+AProgram& AProgram::operator=(const AProgram& other) {
+    ListExpr_ = other.ListExpr_;
+    return *this;
+}
+
+AProgram::AProgram(ListExpr&& _1)
+    : ListExpr_(std::move(_1)) {}
+
+Abstraction::Abstraction(const Abstraction& other)
+    : Ident_(other.Ident_), Expr_(std::make_unique<Expr>(*other.Expr_)) {}
+
+Abstraction& Abstraction::operator=(const Abstraction& other) {
+    Ident_ = other.Ident_;
+    Expr_ = std::make_unique<Expr>(*other.Expr_);
+    return *this;
+}
+
+Abstraction::Abstraction(Ident&& _1, Expr&& _2)
+    : Ident_(std::move(_1)), Expr_(std::make_unique<Expr>(std::move(_2))) {}
+
+Application::Application(const Application& other)
+    : Expr_1(std::make_unique<Expr>(*other.Expr_1))
+    , Expr_2(std::make_unique<Expr>(*other.Expr_2)) {}
+
+Application& Application::operator=(const Application& other) {
+    Expr_1 = std::make_unique<Expr>(*other.Expr_1);
+    Expr_2 = std::make_unique<Expr>(*other.Expr_2);
+    return *this;
+}
+
+Application::Application(Expr&& _1, Expr&& _2)
+    : Expr_1(std::make_unique<Expr>(std::move(_1)))
+    , Expr_2(std::make_unique<Expr>(std::move(_2))) {}
+
+Variable::Variable(const Variable& other)
+    : Ident_(other.Ident_) {}
+
+Variable& Variable::operator=(const Variable& other) {
+    Ident_ = other.Ident_;
+    return *this;
+}
+
+Variable::Variable(Ident&& _1)
+    : Ident_(std::move(_1)) {}
+
+}
--- desired/desired.mk
+++ desired/desired.mk
@@ -5,13 +5,26 @@ desired/grammar.tab.hpp desired/grammar.tab.cpp &: desired/grammar.ypp \
 	| desired
 	cd desired && bison grammar.ypp
 
-desired:
-	mkdir desired
+desired/Absyn.o: desired/Absyn.cpp desired/Absyn.hpp | desired
+	$(CXX) $(CXXFLAGS) -c -o $@ $<
+
+desired/grammar.tab.o: desired/grammar.tab.cpp desired/Absyn.hpp | desired
+	$(CXX) $(CXXFLAGS) -c -o $@ $<
+
+desired/grammar.lex.o: desired/grammar.lex.cpp | desired
+	$(CXX) $(CXXFLAGS) -c -o $@ $<
+
+desired/Test.o: desired/Test.cpp desired/Absyn.hpp desired/grammar.tab.hpp \
+	| desired
+	$(CXX) $(CXXFLAGS) -c -o $@ $<
 
 desired/Test: desired/Test.o desired/grammar.tab.o \
 	desired/grammar.lex.o desired/Absyn.o | desired
-	cd desired && $(CXX) $(CXXFLAGS) Test.o grammar.lex.o grammar.tab.o \
+	cd desired && $(CXX) $(LDFLAGS) Test.o grammar.lex.o grammar.tab.o \
 		Absyn.o -o Test
+
+desired:
+	mkdir desired
 
 all: \
 	desired/Test
