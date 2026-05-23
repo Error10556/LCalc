d754a8026e1395fbcfdbd407de33f163a104be01
Timur Usmanov <t.usmanov@innopolis.university>
2026-05-23 14:35:35 +0300

    Fix yyextra, add debug flags

--- desired/desired.mk
+++ desired/desired.mk
@@ -1,3 +1,7 @@
+DESIRED_CXX = clang++
+DESIRED_CXXFLAGS = -glldb -fsanitize=address -fsanitize=leak
+DESIRED_LDFLAGS = -fsanitize=address -fsanitize=leak
+
 desired/grammar.lex.cpp: desired/grammar.l | desired
 	cd desired && flex grammar.l
 
@@ -6,28 +10,28 @@ desired/grammar.tab.hpp desired/grammar.tab.cpp &: desired/grammar.ypp \
 	cd desired && bison grammar.ypp
 
 desired/Absyn.o: desired/Absyn.cpp desired/Absyn.hpp | desired
-	$(CXX) $(CXXFLAGS) -c -o $@ $<
+	$(CXX) $(CXXFLAGS) $(DESIRED_CXXFLAGS) -c -o $@ $<
 
 desired/grammar.tab.o: desired/grammar.tab.cpp desired/Absyn.hpp | desired
-	$(CXX) $(CXXFLAGS) -c -o $@ $<
+	$(CXX) $(CXXFLAGS) $(DESIRED_CXXFLAGS) -c -o $@ $<
 
 desired/grammar.lex.o: desired/grammar.lex.cpp | desired
-	$(CXX) $(CXXFLAGS) -c -o $@ $<
+	$(CXX) $(CXXFLAGS) $(DESIRED_CXXFLAGS) -c -o $@ $<
 
 desired/PrettyPrinter.o: desired/PrettyPrinter.cpp desired/PrettyPrinter.hpp
-	$(CXX) $(CXXFLAGS) -c -o $@ $<
+	$(CXX) $(CXXFLAGS) $(DESIRED_CXXFLAGS) -c -o $@ $<
 
 desired/SyntaxPrinter.o: desired/SyntaxPrinter.cpp desired/SyntaxPrinter.hpp
-	$(CXX) $(CXXFLAGS) -c -o $@ $<
+	$(CXX) $(CXXFLAGS) $(DESIRED_CXXFLAGS) -c -o $@ $<
 
 desired/Test.o: desired/Test.cpp desired/Absyn.hpp desired/grammar.tab.hpp \
 	desired/PrettyPrinter.hpp desired/SyntaxPrinter.hpp | desired
-	$(CXX) $(CXXFLAGS) -c -o $@ $<
+	$(CXX) $(CXXFLAGS) $(DESIRED_CXXFLAGS) -c -o $@ $<
 
 desired/Test: desired/Test.o desired/grammar.tab.o desired/PrettyPrinter.o \
 	desired/SyntaxPrinter.o desired/grammar.lex.o desired/Absyn.o | desired
-	cd desired && $(CXX) $(LDFLAGS) Test.o grammar.lex.o grammar.tab.o \
-		PrettyPrinter.o SyntaxPrinter.o Absyn.o -o Test
+	cd desired && $(CXX) $(LDFLAGS) $(DESIRED_LDFLAGS) Test.o grammar.lex.o \
+		grammar.tab.o PrettyPrinter.o SyntaxPrinter.o Absyn.o -o Test
 
 desired:
 	mkdir desired
