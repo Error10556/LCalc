cba2ccfa006734fbd91517b2284b123d49428612
Timur Usmanov <t.usmanov@innopolis.university>
2026-05-23 13:13:48 +0300

    Do not clone nullptr in absyn.cpp

--- desired/Absyn.cpp
+++ desired/Absyn.cpp
@@ -3,6 +3,12 @@
 
 namespace LC {
 
+template <class T>
+static std::unique_ptr<T> ClonePtr(const std::unique_ptr<T>& p) {
+    if (!p) return {};
+    return std::make_unique<T>(*p);
+}
+
 Ident::Ident(const std::string& s)
     : String(s) {}
 
@@ -31,11 +37,11 @@ AProgram::AProgram(ListExpr&& _1)
     : ListExpr_(std::move(_1)) {}
 
 Abstraction::Abstraction(const Abstraction& other)
-    : Ident_(other.Ident_), Expr_(std::make_unique<Expr>(*other.Expr_)) {}
+    : Ident_(other.Ident_), Expr_(ClonePtr<Expr>(other.Expr_)) {}
 
 Abstraction& Abstraction::operator=(const Abstraction& other) {
     Ident_ = other.Ident_;
-    Expr_ = std::make_unique<Expr>(*other.Expr_);
+    Expr_ = ClonePtr<Expr>(other.Expr_);
     return *this;
 }
 
@@ -43,12 +49,12 @@ Abstraction::Abstraction(Ident&& _1, Expr&& _2)
     : Ident_(std::move(_1)), Expr_(std::make_unique<Expr>(std::move(_2))) {}
 
 Application::Application(const Application& other)
-    : Expr_1(std::make_unique<Expr>(*other.Expr_1))
-    , Expr_2(std::make_unique<Expr>(*other.Expr_2)) {}
+    : Expr_1(ClonePtr<Expr>(other.Expr_1))
+    , Expr_2(ClonePtr<Expr>(other.Expr_2)) {}
 
 Application& Application::operator=(const Application& other) {
-    Expr_1 = std::make_unique<Expr>(*other.Expr_1);
-    Expr_2 = std::make_unique<Expr>(*other.Expr_2);
+    Expr_1 = ClonePtr<Expr>(other.Expr_1);
+    Expr_2 = ClonePtr<Expr>(other.Expr_2);
     return *this;
 }
 
