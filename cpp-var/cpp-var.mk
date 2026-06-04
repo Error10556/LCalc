ed5117b8270cac7f6fef3425da5c30d5a92251ff
Timur Usmanov <t.usmanov@innopolis.university>
2026-06-04 13:14:14 +0300

    Add recipes for grammar.ypp

--- cpp-var/cpp-var.mk
+++ cpp-var/cpp-var.mk
@@ -3,7 +3,7 @@ VAR_CXXFLAGS = -glldb -fsanitize=address -fsanitize=leak
 VAR_LDFLAGS = -fsanitize=address -fsanitize=leak
 VAR_COMPILE = $(VAR_CXX) $(VAR_CXXFLAGS) -c -o
 
-cpp-var/Absyn.hpp cpp-var/Absyn.cpp &: grammar.cf | cpp-var
+cpp-var/Absyn.hpp cpp-var/Absyn.cpp cpp-var/grammar.l cpp-var/grammar.ypp &: grammar.cf | cpp-var
 	cd cpp-var && bnfc --cpp-var -p LC ../grammar.cf
 
 cpp-var/Absyn.o: cpp-var/Absyn.cpp cpp-var/Absyn.hpp | cpp-var
@@ -19,3 +19,5 @@ clean-var:
 	test ! -f cpp-var/Absyn.o || rm cpp-var/Absyn.o
 	test ! -f cpp-var/Absyn.cpp || rm cpp-var/Absyn.cpp
 	test ! -f cpp-var/Absyn.hpp || rm cpp-var/Absyn.hpp
+	test ! -f cpp-var/grammar.l || rm cpp-var/grammar.l
+	test ! -f cpp-var/grammar.ypp || rm cpp-var/grammar.ypp
