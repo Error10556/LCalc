427481e792adfa9f65da92568dc157eca56e8a3a
Timur Usmanov <t.usmanov@innopolis.university>
2026-06-09 17:17:45 +0300

    Improve PrettyPrint.cpp

--- desired/PrettyPrinter.cpp
+++ desired/PrettyPrinter.cpp
@@ -1,19 +1,23 @@
 /**** Disclaimer ****
  * The default PrettyPrinter implementation makes a number of assumptions about
  * the target language. Namely:
+ *
  * * All tokens are separated from each other by a space, except for commas ',',
- *   which are only separated from the right, and brackets '[]' or
- *   parentheses '()', which are not separated from the enclosed text.
+ *   and semicolons ';', which are only separated from the right, and brackets
+ *  '[]' or parentheses '()', which are not separated from the enclosed text.
+ *
  * * Curly braces '{}' (and only those) enclose an indented block. The
- * indentation equals 4 spaces. The left curly brace causes one line break
- * _after_ itself, the right one causes one _before_ itself. The right brace is
- * not indented.
+ *   indentation equals 4 spaces. The left curly brace causes one line break
+ *   _after_ itself, the right one causes breaks _before and after_ itself. The
+ *   right brace is not indented.
+ *
  * * Semicolons ';' cause a line break after themselves.
+ *
  * * Precedence is always resolved by enclosing a term in parentheses '()'.
  *
  * Since the above limitations likely make the pretty-printed code look far from
- * pretty, you are encouraged to patch this file with your own implementations
- * of some methods.
+ * pretty, you are encouraged to _PATCH(1)_ this file with your own
+ * implementations of some methods.
  */
 
 #include "PrettyPrinter.hpp"
@@ -21,6 +25,11 @@
 #include "Absyn.hpp"
 
 namespace LC {
+
+#define IF_BAD_COERC(classname) \
+    if (coercionLevel > reflection::CoercionLevel<classname>)
+
+constexpr static const unsigned int INDENT = 4;
 
 PrettyPrinter::PrettyPrinter(std::ostream& out, unsigned int indent,
                              int coercionLevel)
@@ -50,57 +59,54 @@ PrettyPrinter PrettyPrinter::Dedented(unsigned int minusIndent,
 void PrettyPrinter::operator()(const Program& v) const { std::visit(*this, v); }
 
 void PrettyPrinter::operator()(const ListExpr& v) const {
-    if (coercionLevel > reflection::CoercionLevel<ListExpr>) out << '(';
+    IF_BAD_COERC(ListExpr) out << '(';
     PrettyPrinter itemprinter = WithCoercionLevel(0);
     auto iter = v.begin();
     auto end = v.end();
     if (iter != end) itemprinter(*iter++);
     for (; iter != end; ++iter) {
-        out << " ;";
+        out << ";";
         NewLine();
         itemprinter(*iter);
     }
-    if (coercionLevel > reflection::CoercionLevel<ListExpr>) out << ')';
+    IF_BAD_COERC(ListExpr) out << ')';
 }
 
 void PrettyPrinter::operator()(const Expr& v) const { std::visit(*this, v); }
 
 void PrettyPrinter::operator()(const AProgram& v) const {
-    if (coercionLevel > reflection::CoercionLevel<AProgram>) out << '(';
+    IF_BAD_COERC(AProgram) out << '(';
     WithCoercionLevel(0)(v.ListExpr_);
-    if (coercionLevel > reflection::CoercionLevel<AProgram>) out << ')';
+    IF_BAD_COERC(AProgram) out << ')';
 }
 
 void PrettyPrinter::operator()(const Abstraction& v) const {
-    if (coercionLevel > reflection::CoercionLevel<Abstraction>) out << '(';
-    out << 'l';
-    out << ' ';
+    IF_BAD_COERC(Abstraction) out << '(';
+    out << "( ";
     WithCoercionLevel(0)(v.Ident_);
-    out << ' ';
-    out << '.';
-    out << ' ';
+    out << " . ";
     std::visit(WithCoercionLevel(0), *v.Expr_);
-    if (coercionLevel > reflection::CoercionLevel<Abstraction>) out << ')';
+    IF_BAD_COERC(Abstraction) out << ')';
 }
 
 void PrettyPrinter::operator()(const Application& v) const {
-    if (coercionLevel > reflection::CoercionLevel<Application>) out << '(';
+    IF_BAD_COERC(Application) out << '(';
     WithCoercionLevel(1)(*v.Expr_1);
     out << ' ';
     WithCoercionLevel(2)(*v.Expr_2);
-    if (coercionLevel > reflection::CoercionLevel<Application>) out << ')';
+    IF_BAD_COERC(Application) out << ')';
 }
 
 void PrettyPrinter::operator()(const Variable& v) const {
-    if (coercionLevel > reflection::CoercionLevel<Variable>) out << '(';
+    IF_BAD_COERC(Variable) out << '(';
     WithCoercionLevel(0)(v.Ident_);
-    if (coercionLevel > reflection::CoercionLevel<Variable>) out << ')';
+    IF_BAD_COERC(Variable) out << ')';
 }
 
 void PrettyPrinter::operator()(const Ident& v) const {
-    if (coercionLevel > reflection::CoercionLevel<Ident>) out << '(';
+    IF_BAD_COERC(Ident) out << '(';
     out << v.String;
-    if (coercionLevel > reflection::CoercionLevel<Ident>) out << ')';
+    IF_BAD_COERC(Ident) out << ')';
 }
 
 #define PrettyPrinterSHL(type)                                               \
@@ -116,6 +122,7 @@ PrettyPrinterSHL(AProgram);
 PrettyPrinterSHL(Abstraction);
 PrettyPrinterSHL(Application);
 PrettyPrinterSHL(Variable);
+PrettyPrinterSHL(Ident);
 
 const PrettyPrinter& operator<<(const PrettyPrinter& p, std::string_view v) {
     p.out << v;
