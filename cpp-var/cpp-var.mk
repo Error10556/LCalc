0063fb6eb217816b3cc0ca34dd57cd0d39cf5314
Timur Usmanov <t.usmanov@innopolis.university>
2026-06-26 21:06:35 +0300

    Add PrinterCommon to recipes

--- cpp-var/cpp-var.mk
+++ cpp-var/cpp-var.mk
@@ -4,6 +4,7 @@ VAR_LDFLAGS = -fsanitize=address -fsanitize=leak
 VAR_COMPILE = $(VAR_CXX) $(VAR_CXXFLAGS) -c -o
 
 cpp-var/Absyn.hpp cpp-var/Absyn.cpp cpp-var/grammar.l cpp-var/grammar.ypp \
+	cpp-var/PrinterCommon.hpp cpp-var/PrinterCommon.cpp \
 	cpp-var/PatternMatching.hpp cpp-var/SyntaxPrinter.cpp \
 	cpp-var/SyntaxPrinter.hpp cpp-var/PrettyPrinter.cpp \
 	cpp-var/PrettyPrinter.hpp cpp-var/Test.cpp &: grammar.cf | cpp-var
@@ -24,10 +25,15 @@ cpp-var/grammar.lex.o: cpp-var/grammar.lex.cpp cpp-var/grammar.tab.hpp | cpp-var
 cpp-var/grammar.tab.o: cpp-var/grammar.tab.cpp cpp-var/PatternMatching.hpp cpp-var/Absyn.hpp | cpp-var
 	$(VAR_COMPILE) $@ -Icpp-var $<
 
-cpp-var/SyntaxPrinter.o: cpp-var/SyntaxPrinter.cpp cpp-var/SyntaxPrinter.hpp | cpp-var
+cpp-var/PrinterCommon.o: cpp-var/PrinterCommon.cpp cpp-var/PrinterCommon.hpp | cpp-var
 	$(VAR_COMPILE) $@ -Icpp-var $<
 
-cpp-var/PrettyPrinter.o: cpp-var/PrettyPrinter.cpp cpp-var/PrettyPrinter.hpp | cpp-var
+cpp-var/SyntaxPrinter.o: cpp-var/SyntaxPrinter.cpp cpp-var/SyntaxPrinter.hpp \
+	cpp-var/PrinterCommon.hpp | cpp-var
+	$(VAR_COMPILE) $@ -Icpp-var $<
+
+cpp-var/PrettyPrinter.o: cpp-var/PrettyPrinter.cpp cpp-var/PrettyPrinter.hpp \
+	cpp-var/PrinterCommon.hpp | cpp-var
 	$(VAR_COMPILE) $@ -Icpp-var $<
 
 cpp-var/Test.o: cpp-var/Test.cpp cpp-var/Absyn.hpp cpp-var/grammar.tab.hpp \
@@ -35,10 +41,10 @@ cpp-var/Test.o: cpp-var/Test.cpp cpp-var/Absyn.hpp cpp-var/grammar.tab.hpp \
 	$(VAR_COMPILE) $@ -Icpp-var $<
 
 cpp-var/Test: cpp-var/Test.o cpp-var/Absyn.o cpp-var/grammar.lex.o cpp-var/grammar.tab.o \
-	cpp-var/SyntaxPrinter.o cpp-var/PrettyPrinter.o | cpp-var
+	cpp-var/PrinterCommon.o cpp-var/SyntaxPrinter.o cpp-var/PrettyPrinter.o | cpp-var
 	cd cpp-var && $(VAR_CXX) $(VAR_LDFLAGS) $(LDFLAGS) \
-		Test.o Absyn.o grammar.lex.o grammar.tab.o SyntaxPrinter.o \
-		PrettyPrinter.o -o Test
+		Test.o Absyn.o grammar.lex.o grammar.tab.o PrinterCommon.o \
+		SyntaxPrinter.o PrettyPrinter.o -o Test
 
 cpp-var:
 	mkdir cpp-var
@@ -58,6 +64,9 @@ clean-var:
 	@ test ! -f cpp-var/grammar.tab.cpp || rm cpp-var/grammar.tab.cpp
 	@ test ! -f cpp-var/grammar.tab.hpp || rm cpp-var/grammar.tab.hpp
 	@ test ! -f cpp-var/grammar.tab.o || rm cpp-var/grammar.tab.o
+	@ test ! -f cpp-var/PrinterCommon.cpp || rm cpp-var/PrinterCommon.cpp
+	@ test ! -f cpp-var/PrinterCommon.hpp || rm cpp-var/PrinterCommon.hpp
+	@ test ! -f cpp-var/PrinterCommon.o || rm cpp-var/PrinterCommon.o
 	@ test ! -f cpp-var/SyntaxPrinter.cpp || rm cpp-var/SyntaxPrinter.cpp
 	@ test ! -f cpp-var/SyntaxPrinter.hpp || rm cpp-var/SyntaxPrinter.hpp
 	@ test ! -f cpp-var/SyntaxPrinter.o || rm cpp-var/SyntaxPrinter.o
