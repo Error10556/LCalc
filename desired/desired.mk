849eb508c499666f66de20747989f6dcc8846872
Timur Usmanov <t.usmanov@innopolis.university>
2026-07-08 15:06:41 +0300

    Add debugging to desired.mk

--- desired/desired.mk
+++ desired/desired.mk
@@ -1,6 +1,6 @@
 DESIRED_CXX = clang++
-DESIRED_CXXFLAGS = -std=c++17 -Wall -Wextra -Wno-unused-but-set-variable -fsanitize=address -fsanitize=leak
-DESIRED_LDFLAGS = -fsanitize=address -fsanitize=leak
+DESIRED_CXXFLAGS = -std=c++17 -Wall -Wextra -Wno-unused-but-set-variable -fsanitize=address -fsanitize=leak -fstandalone-debug -glldb
+DESIRED_LDFLAGS = -fsanitize=address -fsanitize=leak -fstandalone-debug -glldb
 
 all: desired/Test desired/Eval
 
